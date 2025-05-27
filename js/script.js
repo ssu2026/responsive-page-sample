5cf890cb77c74590743294afea243a3aebaa4d0e
ssu2026 <yeona2026@gmail.com>
2025-05-27 13:06:25 +0900

    script.js 코드 수정

--- js/script.js
+++ js/script.js
@@ -1,30 +1,32 @@
-// Isotope 인스턴스 객체 생성
-const sectionIso = new Isotope(".section", {
-  // options
-  itemSelector: "article",
-  masonry: {
-    // use element for option
-    columnWidth: "article",
-  },
-});
+window.addEventListener("load", () => {
+  // Isotope 인스턴스 객체 생성
+  const sectionIso = new Isotope(".section", {
+    // options
+    itemSelector: "article",
+    masonry: {
+      // use element for option
+      columnWidth: "article",
+    },
+  });
 
-const sortingBtns = document.querySelectorAll(".sorting li");
-sortingBtns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
+  const sortingBtns = document.querySelectorAll(".sorting li");
+  sortingBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
 
-    // // 재 정렬
-    const filter = e.currentTarget.querySelector("a").getAttribute("href");
+      // // 재 정렬
+      const filter = e.currentTarget.querySelector("a").getAttribute("href");
 
-    sectionIso.arrange({
-      filter: filter,
+      sectionIso.arrange({
+        filter: filter,
+      });
+
+      sortingBtns.forEach((btnEl) => {
+        btnEl.classList.remove("on");
+      });
+
+      e.currentTarget.classList.add("on");
     });
-
-    sortingBtns.forEach((btnEl) => {
-      btnEl.classList.remove("on");
-    });
-
-    e.currentTarget.classList.add("on");
   });
 });
 
